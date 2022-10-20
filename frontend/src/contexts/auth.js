8e5745d403994b36aa6ddd013f9006409344d102
Rhenan <diasrhenan@gmail.com>
2022-10-20 09:57:23 -0300

    Adicionando Bypass de autenticação

--- frontend/src/contexts/auth.js
+++ frontend/src/contexts/auth.js
@@ -2,12 +2,22 @@ import React from "react";
 
 const AuthContext = React.createContext();
 
+const useAuth = process.env.REACT_APP_USE_AUTH === "false" ? false : true;
+
 function createContext(cognitoObject) {
-  return {
-    accessToken: cognitoObject.signInUserSession.accessToken.jwtToken,
-    userName: cognitoObject.attributes["custom:displayname"],
-    sub: cognitoObject.attributes.sub,
-  };
+  if (useAuth) {
+    return {
+      accessToken: cognitoObject.signInUserSession.accessToken.jwtToken,
+      userName: cognitoObject.attributes["custom:displayname"],
+      sub: cognitoObject.attributes.sub,
+    };
+  } else {
+    return {
+      accessToken: null,
+      userName: "Sherlock Holmes",
+      sub: null,
+    };
+  }
 }
 
 export { AuthContext, createContext };
