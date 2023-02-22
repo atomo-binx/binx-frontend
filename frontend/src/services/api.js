d328b806a63df1b8109c54b6c24f306d4d3cdcd0
rhenandias <diasrhenan@gmail.com>
2023-02-22 15:19:02 -0300

    Correções no design do loading da tela de método de frete

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -16,6 +16,6 @@ let baseURL =
     ? process.env.REACT_APP_LOCAL_API_URL
     : "https://api.binx.com.br/api";
 
-const api = axios.create({ baseURL: baseURL });
+const api = axios.create({ baseURL: baseURL, timeout: 180000 });
 
 export default api;
