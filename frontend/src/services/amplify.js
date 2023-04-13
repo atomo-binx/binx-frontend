2585b2781374978fb75b92c02cd111e97dfe5d26
rhenandias <diasrhenan@gmail.com>
2023-04-13 18:15:30 -0300

    Corrigindo importação de CSS

--- frontend/src/services/amplify.js
+++ frontend/src/services/amplify.js
@@ -4,7 +4,7 @@ Amplify.configure({
   Auth: {
     region: process.env.REACT_APP_REGION,
     userPoolId: process.env.REACT_APP_USER_POOL_ID,
-    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID
+    userPoolWebClientId: process.env.REACT_APP_USER_POOL_WEB_CLIENT_ID,
   },
 });
 
